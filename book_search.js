ae7ca4b28c9cc714c822445edd1092dbea500920
Katie Taylor <katiemxtaylor@gmail.com>
2023-12-08 19:36:14 -0800

    add initial unit tests

--- book_search.js
+++ book_search.js
@@ -30,6 +30,9 @@
     return result; 
 }
 
+/**
+ * EXAMPLE INPUT & OUTPUT OBJECTS
+ */
 /** Example input object. */
 const twentyLeaguesIn = [
     {
@@ -67,6 +70,116 @@ const twentyLeaguesOut = {
     ]
 }
 
+/** Example output object with multiple results given twentyLeaguesIn */
+const twentyLeaguesMultipleResultsOut = {
+    "SearchTerm": "and",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        },
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 10
+        }
+    ]
+}
+
+/** Example input object with multiple books */
+const multipleBooksIn = [
+    {
+        "Title": "Twenty Thousand Leagues Under the Sea",
+        "ISBN": "9780000528531",
+        "Content": [
+            {
+                "Page": 31,
+                "Line": 8,
+                "Text": "now simply went on by her own momentum.  The dark-"
+            },
+            {
+                "Page": 31,
+                "Line": 9,
+                "Text": "ness was then profound; and however good the Canadian\'s"
+            },
+            {
+                "Page": 31,
+                "Line": 10,
+                "Text": "eyes were, I asked myself how he had managed to see, and"
+            } 
+        ] 
+    },
+    {
+        "Title": "Recoding America",
+        "ISBN": "9781250266774",
+        "Content": [
+            {
+                "Page": 23,
+                "Line": 1,
+                "Text": "Over the past decade, I have seen technology fail at all"
+            },
+            {
+                "Page": 23,
+                "Line": 2,
+                "Text": "levels of government. Sometimes it happens loudly and spectac-"
+            },
+            {
+                "Page": 23,
+                "Line": 3,
+                "Text": "ularly, as when healthcare.gov crashed on launch; sometimes it's"
+            } 
+        ] 
+    }
+];
+
+/** Example input object with multiple results from multiple books input */
+const multipleBooksMultipleResultsOut = {
+    "SearchTerm": "I",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 10
+        },
+        {
+            "ISBN": "9781250266774",
+            "Page": 23,
+            "Line": 1
+        },
+    ]
+};
+
+/** Example input object with empty book list for my unit tests */
+const emptyBookListIn = [{}];
+
+/** Example input object with book with empty content for my unit tests */
+const emptyContentIn = [
+    {
+        "Title": "The Empty Book",
+        "ISBN": "123456789",
+        "Content": [] 
+    }
+];
+
+/** Example output object with blank (but unaltered) search term (& thus no results) */
+const blankSearchTermOut = {
+    "SearchTerm": "  ",
+    "Results": []
+};
+
+/** Example output object for use with example input objects with corresponding hyphenated words */
+const hyphenatedSearchTermOut = {
+    "SearchTerm": "darkness",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
+        }
+    ]
+};
+
 /*
  _   _ _   _ ___ _____   _____ _____ ____ _____ ____  
 | | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___| 
@@ -101,4 +214,67 @@ if (test2result.Results.length == 1) {
     console.log("FAIL: Test 2");
     console.log("Expected:", twentyLeaguesOut.Results.length);
     console.log("Received:", test2result.Results.length);
+}
+
+/** Test that search can find multiple results given a single-book input */
+const test3result = findSearchTermInBooks("and", multipleBooksIn); 
+if (JSON.stringify(twentyLeaguesMultipleResultsOut) === JSON.stringify(test3result)) {
+    console.log("PASS: Test 3");
+} else {
+    console.log("FAIL: Test 3");
+    console.log("Expected:", twentyLeaguesMultipleResultsOut);
+    console.log("Received:", test3result);
+}
+
+/** Test that search can find multiple results given a multiple-book input */
+const test4result = findSearchTermInBooks("I", multipleBooksIn); 
+if (JSON.stringify(multipleBooksMultipleResultsOut) === JSON.stringify(test4result)) {
+    console.log("PASS: Test 4");
+} else {
+    console.log("FAIL: Test 4");
+    console.log("Expected:", multipleBooksMultipleResultsOut);
+    console.log("Received:", test4result);
+}
+
+/**
+ * EDGE CASE TESTS
+ */
+/** Test that the search can handle a list with no books (and not find any matches, of course) */
+const test5result = findSearchTermInBooks("the", emptyBookListIn);
+if (test5result.Results.length == 0) { // search term not important to check in this case
+    console.log("PASS: Test 5");
+} else {
+    console.log("FAIL: Test 5");
+    console.log("Expected:", 0);
+    console.log("Received:", test5result.Results.length);
+}
+
+/** Test that the search can handle a book with no scanned content (and not find any matches, of course) */
+const test6result = findSearchTermInBooks("the", emptyContentIn);
+if (test6result.Results.length == 0) {
+    console.log("PASS: Test 6");
+} else {
+    console.log("FAIL: Test 6");
+    console.log("Expected:", 0);
+    console.log("Received:", test6result.Results.length);
+}
+
+/** Test that search can handle a search term that's whitespace or otherwise blank */
+const test7result = findSearchTermInBooks("  ", twentyLeaguesIn);
+if (JSON.stringify(blankSearchTermOut) === JSON.stringify(test7result)) {
+    console.log("PASS: Test 7");
+} else {
+    console.log("FAIL: Test 7");
+    console.log("Expected:", blankSearchTermOut);
+    console.log("Received:", test7result);
+}
+
+/** Test that search can handle a search term that includes a result from a hyphenated version of the term. This is an edge case, and if I don't get to implementing a solution for this case, I'll comment it out. */
+const test8result = findSearchTermInBooks("darkness", twentyLeaguesIn);
+if (JSON.stringify(hyphenatedSearchTermOut) === JSON.stringify(test8result)) {
+    console.log("PASS: Test 8");
+} else {
+    console.log("FAIL: Test 8");
+    console.log("Expected:", hyphenatedSearchTermOut);
+    console.log("Received:", test8result);
 }
